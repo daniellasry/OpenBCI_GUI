8c5b781ba61c8ba87568c48b3d7cf707b7fecb5b
Daniel Lasry <daniellasry@gmail.com>
2020-04-30 19:23:44 -0700

    not output dir

--- release_script/make-release.py
+++ release_script/make-release.py
@@ -106,7 +106,7 @@ def build_app(sketch_dir, processing_command):
     # so we can't reliably check for success or failure
     print ("Using sketch: " + sketch_dir)
     print ("Using command: " + processing_command)
-    subprocess.call([processing_command, "--sketch=" + sketch_dir, "--output=" + os.getcwd(), "--export"])
+    subprocess.call([processing_command, "--sketch=" + sketch_dir, "--export"])
 
 ### Function: Package the app in the expected file structure
 ###########################################################
