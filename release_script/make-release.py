80c0d6573d759a1a4c734b1502115c61efd74961
Daniel Lasry <daniellasry@gmail.com>
2020-04-30 17:49:54 -0700

    remove python 2 stuff

--- release_script/make-release.py
+++ release_script/make-release.py
@@ -19,10 +19,6 @@ import platform
 import subprocess
 import argparse
 
-# Fix Python 2.x.
-try: input = raw_input
-except NameError: pass
-
 ### Define platform-specific strings
 ###########################################################
 MAC = 'Darwin'
