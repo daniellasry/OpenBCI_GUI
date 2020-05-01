664bc667d3c2a0e4a187fcabe838ab992fb1fa77
Daniel Lasry <daniellasry@gmail.com>
2020-04-30 18:26:13 -0700

    build a different way

--- release_script/make-release.py
+++ release_script/make-release.py
@@ -252,11 +252,11 @@ def main ():
 
     # run the build (processing-java)
     processing_command = args.processing_command
-    build_app(sketch_dir, processing_command)
+    #build_app(sketch_dir, processing_command)
 
     #package it up
-    flavor = flavors[LOCAL_OS]
-    package_app(sketch_dir, flavor, windows_signing, windows_pfx_path, windows_pfx_password)
+    #flavor = flavors[LOCAL_OS]
+    #package_app(sketch_dir, flavor, windows_signing, windows_pfx_path, windows_pfx_password)
 
 if __name__ == "__main__":
     main ()
